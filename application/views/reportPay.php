dccc2ee073a406796caf6d6d2f50ca7bcc7eeb4a
SupapornZiiBow <ziibow.supaporn@gmail.com>
2015-05-05 14:51:17 +0700

    edit View

--- application/views/reportPay.php
+++ application/views/reportPay.php
@@ -11,43 +11,42 @@
   <div id="searchResult">
 <table width="80%" border="0" align="center" cellpadding="7" cellspacing="3">
     <tr> 
-    <th align="center" valign="baseline" nowrap="nowrap" style="font-size: 12px"><p>ชื่อ - สกุล</p></th>  
-    	
-    	
+        <th align="center" valign="baseline" nowrap="nowrap" style="font-size: 12px"><p>ชื่อ - สกุล</p></th>  
         <th align="center" valign="baseline" nowrap="nowrap" style="font-size: 12px"><p>วันที่</p></th>
-        
         <th align="center" valign="baseline" nowrap="nowrap" style="font-size: 12px">ประเภทกองทุน</th>
-   	    <th align="center" valign="baseline" nowrap="nowrap" style="font-size: 12px"><p>จำนวนเงิน </p></th>
-       
+        <th align="center" valign="baseline" nowrap="nowrap" style="font-size: 12px"><p>จำนวนเงิน </p></th>      
     </tr>
-    <?php
-	if($listMember){
-	$sum = 0;
-	 foreach($listMember as $m){
-		 $sum = $sum+$m['loanNum'];
-		 ?>
-    <tr>      
-    <td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p><?php echo $m['memberName'];?>&nbsp;&nbsp;<?php echo $m['memberLastname'];?></p></td>   
-       
-       
-       <td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p ><?php $date = new DateTime($m['loanDate']);
-echo $date->format('d-m-Y');?></p></td>
-	    
-  
     
-      <td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p><?php echo $m['typeName'];?></p></td>
-      
-      <td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p><?php echo $m['loanNum'];?></p></td> 
-     
-  
+    <?php
+		if($listMember){
+			$sum = 0;
+	 			foreach($listMember as $m){
+					 $sum = $sum+$m['loanNum'];
+	?>
+    
+    <tr>      
+    	<td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p>
+			<?php echo $m['memberName'];?>&nbsp;&nbsp;<?php echo $m['memberLastname'];?></p>
+        </td>   
+        <td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p >
+			<?php $date = new DateTime($m['loanDate']);echo $date->format('d-m-Y');?></p>
+        </td>
+       <td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p>
+	  		<?php echo $m['typeName'];?></p>
+       </td>
+       <td align="center" valign="middle" nowrap="nowrap" style="font-size: 12px"><p>
+	   		<?php echo $m['loanNum'];?></p>
+       </td> 
     </tr>
     <?php }?>
-<tr>
-  	<td colspan="8" align="center"><div class="ajax_paging"><?php echo $this->pagination->create_links(); ?></div></td>
+   <tr>
+   		<td colspan="8" align="center"><div class="ajax_paging">
+			<?php echo $this->pagination->create_links(); ?></div>
+       </td>
   </tr>
-   <?php 	}else{?>
+   <?php }else{?>
   	<tr>
-  	<td colspan="8" align="center">ไม่พบข้อมูล</td>
+  		<td colspan="8" align="center">ไม่พบข้อมูล</td>
   </tr>
   <?php }?>
 </table>
